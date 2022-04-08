59eef7b8cec0e33949dcce40d18474fc864db0e6
Andreas Asprou <andyasprou@gmail.com>
2022-04-08 12:34:16 +0700

    add redis

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const { createClient } = require('redis');
 const PORT = process.env.PORT || 5000
 const HOST = process.env.HOST || "0.0.0.0"
 const { MongoClient } = require("mongodb");
@@ -7,17 +8,38 @@ const { MongoClient } = require("mongodb");
 // Create a new MongoClient
 const client = new MongoClient(process.env.MONGODB_URI);
 
-async function start() {
+const connectToRedis = async () => {
+  const client = createClient({
+    url: process.env.REDIS_URL
+  });
+
+  client.on('error', (err) => {
+    console.log('Redis Client Error', err);
+  });
+
+  await client.connect();
+
+  await client.ping()
+
+  console.log('Successfully connected to Redis DB')
+}
+
+const connectToMongoDB = async () => {
   try {
     // Connect the client to the server
     await client.connect();
     // Establish and verify connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
+    console.log("Connected successfully to mongodb server");
   } catch (error) {
     await client.close();
     throw error;
   }
+}
+
+async function start() {
+  await connectToMongoDB();
+  await connectToRedis();
 
   express()
     .use(express.static(path.join(__dirname, 'public')))
